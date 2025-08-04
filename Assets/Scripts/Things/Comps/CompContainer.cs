4e5091957fc018ce72da2cde7d7fc971164f5770
amulford <alex.mulford@gmail.com>
2025-08-04 21:05:08 +0100

    added container comp stub

---
+++ Assets/Scripts/Things/Comps/CompContainer.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+
+[Serializable]
+public partial class CompProperties_Container : CompProperties
+{
+    public int capacity = 10;
+
+    public CompProperties_Container()
+	{
+		compClass = typeof(CompContainer);
+	}
+}
+
+public class CompContainer : ThingComp
+{
+    private List<Thing> contents = new();
+
+    public CompContainer(Thing parent) : base(parent)
+    {
+    }
+
+    public CompContainer(Thing parent, CompProperties props) : base(parent, props)
+	{
+	}
+
+    public void Add(Thing thing)
+    {
+        if( !contents.Contains(thing) )
+            contents.Add(thing);
+    }
+
+    public bool Remove(Thing thing)
+    {
+        return contents.Remove(thing);
+    }
+
+    public override void Tick()
+    {
+    }
+}
+
+#if UNITY_EDITOR
+public partial class CompProperties_Container
+{
+    public override void DrawEditorFields()
+    {
+        capacity = UnityEditor.EditorGUILayout.IntField("Capacity", capacity);
+    }
+}
+#endif
